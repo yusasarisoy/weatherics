3113d22b244ba696b800ea22eb0760b5dd317471
Yuşa Sarısoy <yusasarisoy@icloud.com>
2021-03-13 22:09:54 +0300

    UITextField interactions have been implemented.

--- weatherics/Controller/WeatherViewController.swift
+++ weatherics/Controller/WeatherViewController.swift
@@ -8,17 +8,42 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var textFieldSearch: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textFieldSearch.delegate = self // Assign this class as a delegeate of the UITextField.
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        textFieldSearch.endEditing(true) // End editing, close the keyboard.
+        print(textFieldSearch.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textFieldSearch.endEditing(true) // End editing, close the keyboard.
+        print(textFieldSearch.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textFieldSearch.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // TODO: - Use textFieldSearch.text to get the weather for that city.
+        textFieldSearch.text = ""
+    }
 }
 
